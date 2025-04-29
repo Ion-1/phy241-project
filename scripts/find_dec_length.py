7b1c05f171458f390d54f7bcb3842531015fe370
Cookie265 <cookie2653589@gmail.com>
2025-04-29 11:37:18 +0200

    task 2
    gaussian decay

--- scripts/find_dec_length.py
+++ scripts/find_dec_length.py
@@ -1,9 +1,24 @@
 import numpy as np
+import scipy
+from functools import partial
 
-def nnl(args):
-    mean_k,sigma_k,sigma_pi=args
-    mean_pi=1
-    amount_pi=0.84
-    amount_k=1-amount_pi
-    data=np.loadtxt(".\data\dec_lengths.txt")
-    probs
+def nll(data, mean_k):
+    #print("testing at", mean_k)
+    probs=np.clip(distribution(mean_k,data),1e-10,1)
+    logs=np.log(probs)
+    return -np.sum(logs)
+
+def distribution(mean_k,x):
+    mean_pi = 4188
+    amount_pi = 0.84
+    amount_k = 1 - amount_pi
+    return amount_k*scipy.stats.expon.pdf(x, loc=0, scale=mean_k) + amount_pi*scipy.stats.expon.pdf(x, loc=0, scale=mean_pi)
+
+def task():
+    data = np.loadtxt(".\data\dec_lengths.txt")
+    start=500
+    bounds = (0,5000)
+    best = scipy.optimize.minimize(partial(nll, data), start)
+    return best
+
+print(task())
