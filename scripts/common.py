8991f42e5b87836e85182ccf9df6728160de05d3
Ion Skalamera <skalameraion@gmail.com>
2025-04-30 17:14:56 +0200

    I heard committing broken code is cool
    Gotta get a proper value for the decay length and see if the code works.

--- scripts/common.py
+++ scripts/common.py
@@ -3,9 +3,37 @@ from __future__ import annotations
 import argparse
 import os
 import json
+import base64
 import numpy as np
 from typing import Any, Union
 from numpy.typing import NDArray
+from dataclasses import dataclass
+
+# Physical constants
+@dataclass
+class CONSTANTS:
+    EV_TO_JOULES = 1.602176634e-19
+    SPEED_OF_LIGHT = 299792458 # in ms^-1
+    PION_POS_MASS = 139.57039 # in MeV/c^2
+    PION_NEU_MASS = 134.9768 # in MeV/c^2
+    KAON_MASS = 493.677 # in MeV/c^2
+    KAON_MEANLIFE = 1.2380e-8 # in s
+    # Aliases
+    c = SPEED_OF_LIGHT
+    m_k = KAON_MASS
+    m_pp = PION_POS_MASS
+    m_np = PION_NEU_MASS
+    t_k = KAON_MEANLIFE
+    e = EV_TO_JOULES # elementary charge
+    MeV2mps = 10**6 * EV_TO_JOULES / SPEED_OF_LIGHT
+
+
+@dataclass
+class EXPERIMENTAL_CONSTANTS:
+    DETECTOR2_RADIUS = 2 # in m
+    DETECTOR2_RADIUS_SQUARED = 4
+    # Aliases
+    d2radsq = DETECTOR2_RADIUS_SQUARED
 
 # https://stackoverflow.com/questions/10551117/setting-options-from-environment-variables-when-using-argparse
 class EnvDefault(argparse.Action):
@@ -26,14 +54,18 @@ class Cache:
     def __init__(self, file_or_json: Union[str, bytes]):
         if isinstance(file_or_json, bytes):
             self.data = vc_is_dict(json.loads(file_or_json))
-        if isinstance(file_or_json, str):
+        elif isinstance(file_or_json, str):
             if not os.path.exists(file_or_json):
                 raise EnvironmentError("Value cache file does not seem to exist / has too restrictive permissions")
             with open(file_or_json, 'r') as f:
                 self.data = vc_is_dict(json.load(f))
-        raise TypeError(f"`file_or_json` has invalid type: {type(file_or_json)}. Expected: str | bytes.")
-    def to_hex(self) -> str:
-        return bytes(json.dumps(self.data), "UTF-8").hex()
+        else:
+            raise TypeError(f"`file_or_json` has invalid type: {type(file_or_json)}. Expected: str | bytes.")
+    @classmethod
+    def from_b64(cls, data: str) -> Self:
+        return cls(base64.b64decode(data))
+    def to_b64(self) -> str:
+        return base64.b64encode(bytes(json.dumps(self.data), "UTF-8")).decode()
     def dump(self, filepath):
         with open(filepath, "w") as f:
             json.dump(self.data, f)
@@ -49,6 +81,12 @@ class Cache:
     @not_angled_sample.setter
     def not_angled_sample(self, value: NDArray):
         self.data["not_angled_sample"] = value.tolist()
+    @property
+    def not_angled_ideal_z(self) -> float:
+        return self.data.get("not_angled_ideal_z")
+    @not_angled_ideal_z.setter
+    def not_angled_ideal_z(self, value: float):
+        self.data["not_angled_ideal_z"] = value
 
 def vc_is_dict(a: Any) -> dict:
     if not isinstance(a, dict):
--- scripts/experiment_simulation.py
+++ scripts/experiment_simulation.py
@@ -1,22 +1,102 @@
 import sys
 import numpy as np
 import argparse
-from common import EnvDefault, Cache
+import scipy.stats as st
+import scipy.optimize as opt
+from numpy.random import Generator
+from common import EnvDefault, Cache, CONSTANTS as C, EXPERIMENTAL_CONSTANTS as E
 from numpy.typing import NDArray
 from typing import Union
+from functools import partial
 
 
-def generate_sample(avg_dlength: float) -> NDArray:
-    pass
+def generate_sample_kaon_decay(avg_dlength: float, n: int, rng: Generator) -> NDArray:
+    """
+    Generates a (n, 3, 3) ndarray that represents a sample of K+ decay.
+    The first index indexes the n samples.
+    The second index indexes the (decay vertex, p_pos, p_neu) vector triplet,
+    where p is the momentum vector representing the isotropically distributed
+    (in the Kaon rest frame) momentum of the pions in the lab frame.
+    Decay vertex represents the (0, 0, z) exponentially distributed position
+    of the decay event in the z-direction, with a rate of 1/avg_dlength.
+
+    The beam momentum is extrapolated from the decay length and the known mean life
+    of the Kaon, and the momentum of the pions in the rest frame calculated
+    at runtime and everything else from the constants in common.py.
+    """
+    # decay length \lambda = c * \beta * \gamma * \tau
+    # momentum in MeV/c is \beta * \gamma * mass (in MeV/c^2)
+    βγ = avg_dlength / C.t_k / C.c
+    p_K = βγ * C.m_k
+    E_K = np.sqrt(C.m_k ** 2 + p_K ** 2)
+    # magnitude of pions' momentum stems from four-momentum conservation
+    p_p = np.sqrt((C.m_k ** 2 - (C.m_pp + C.m_np) ** 2) * (C.m_k ** 2 - (C.m_pp - C.m_np) ** 2)) / 2 / C.m_k
+
+    vertex_positions = np.concatenate((np.zeros((n, 2)), np.reshape(rng.exponential(scale=avg_dlength, size=n), (n, 1))), axis=1)
+    isotrope_momenta = p_p * st.uniform_direction.rvs(dim=3, size=n, random_state=rng)
+
+    E_positive_pion = np.sqrt(C.m_pp ** 2 + p_p ** 2)
+    # Giving the positive pion the positive array makes no difference to giving it the neutral one
+    pos_pi_4_momenta = np.concatenate((np.full((n,1), E_positive_pion), isotrope_momenta), axis=1)
+    E_neutral_pion = np.sqrt(C.m_np ** 2 + p_p ** 2)
+    neu_pi_4_momenta = np.concatenate((np.full((n,1), E_neutral_pion), -isotrope_momenta), axis=1)
+
+    γ = E_K / C.m_k
+    lorentz_boost = np.array([
+        [γ, 0, 0, βγ],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [βγ, 0, 0, γ]
+    ])
+    boosted_pos_p_4m = C.MeV2mps * pos_pi_4_momenta @ lorentz_boost.T # If only I could use np.matvec
+    boosted_neu_p_4m = C.MeV2mps * neu_pi_4_momenta @ lorentz_boost.T # but alas, python 3.9
+    return np.stack((vertex_positions, boosted_pos_p_4m[:,1:], boosted_neu_p_4m[:, 1:]), axis=1)
+
+
+def calculate_num_intersects(z: float, a: NDArray) -> int:
+    """
+    Takes an NDArray of shape (n, m, 3), where n are the number of samples,
+    (m, 3) where m >= 2 is the array of vectors with the position vector
+    in index 0. Any following vector is the momentum vector of a particle
+    originating from the position.
+    This function will calculate the number of particles i, where
+    0 <= i <= n*(m-1), are intersecting with a disk at position (0, 0, z).
+    """
+    z_travel = z - a[:, 0, 2]
+
+    # Filter out kaons that decay on/behind the detector
+    mask = z_travel <= 0
+    z_travel, a = z_travel[mask], a[mask]
+
+    # We flatten our momentum vectors into one big array, and repeat z_travel for every mom. vec.
+    z_travel = np.repeat(z_travel, a.shape[1]-1)
+    momentum_vecs = a[:, 1:, :].reshape(-1, 3)
+
+    with np.errstate(divide='ignore'):
+        # We ignore division by 0, as that just means travel perpendicular to our detector, and
+        # float('inf') is not going to be intersecting. (But imagine the probability of a div by 0)
+        travel_time = z_travel / momentum_vecs[:, 2]
+
+    # Filter out the ones going backwards in time
+    mask = travel_time < 0
+    travel_time, momentum_vecs = travel_time[mask], momentum_vecs[mask]
+    radius_sq_travelled = np.sum((momentum_vecs[:, :2] * travel_time[:, np.newaxis] + np.repeat(a[:, 0, :2], a.shape[1]-1, axis=0))**2, axis=1)
+
+    return np.sum(radius_sq_travelled <= E.d2radsq)
 
 
 def main(args: argparse.Namespace) -> Union[int, tuple[int, Cache]]:
-    if hasattr(args, "cache"):
-        cache = Cache(bytes.fromhex(args.cache))
+    if hasattr(args, "cache") and args.cache is not None:
+        cache = Cache.from_b64(args.cache)
     else:
         cache = Cache(args.cache_file)
-    sample = generate_sample(cache.average_decay_length)
+    rng = np.random.default_rng(args.seed)
+    sample = generate_sample_kaon_decay(cache.average_decay_length, 100000, rng)
     cache.not_angled_sample = sample
+    print(calculate_num_intersects(2, sample))
+    optimal_result = opt.minimize(lambda *a, **k: -calculate_num_intersects(*a, **k), np.array([3,]), args=(sample,))
+    print(optimal_result)
+    cache.not_angled_ideal_z = optimal_result['x'][0]
     if args.no_write:
         return 0, cache
     else:
@@ -25,16 +105,20 @@ def main(args: argparse.Namespace) -> Union[int, tuple[int, Cache]]:
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="increase output verbosity")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-c", "--cache-file", action=EnvDefault, envvar='VALUECACHE', default=r".\data\value_cache.json", help="File path of the value cache.")
-    group.add_argument("--cache", help="A hexadecimal bytes representation of a UTF-8 JSON string containing the value cache data. Makes `--no-write` true by default.")
-    parser.add_argument("--no-write", type="store_true", help="Do not write out to the value cache. Additionally, main will return the updated value cache instead.")
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument("-c", "--cache-file", action=EnvDefault, type=str, envvar='VALUECACHE', default=r".\data\value_cache.json", help="File path of the value cache.")
+    group.add_argument("--cache", help="A base64 representation of a UTF-8 JSON string containing the value cache data. Implies `--no-write`.")
+    parser.add_argument("--no-write", action="store_true", help="Do not write out the updated value cache. Additionally, main will return the updated value cache instead.")
+    parser.add_argument("--seed", type=int, help="Seed for the default_rng", default=None)
+
     args = parser.parse_args()
-    if hasattr(args, "cache"):
+    if hasattr(args, "cache") and args.cache is not None:
         args.no_write = True
+
     if args.no_write:
         sys.exit(main(args)[0])
     sys.exit(main(args))
