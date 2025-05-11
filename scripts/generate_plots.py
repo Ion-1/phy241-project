f7ef767e65c786a57e606d47e1b9c69f65861c9e
Ion Skalamera <ion1@skalamera.ch>
2025-05-11 12:37:23 +0200

    Good looking 3D graphs :)

--- scripts/generate_plots.py
+++ scripts/generate_plots.py
@@ -2,15 +2,14 @@ import os
 import sys
 import logging
 import argparse
+
 import numpy as np
 import matplotlib.pyplot as plt
 import vispy.io as io
 
-from vispy import app, gloo, scene
-from vispy.scene import TurntableCamera
-from vispy.scene.visuals import Arrow, Mesh
-from scipy.spatial.distance import num_obs_y
+from vispy import app, scene
 
+from typing import overload, Literal as L, Union
 from numpy.typing import NDArray
 
 from find_dec_length import nll, distribution
@@ -76,58 +75,66 @@ def _plot_nll(cache: Cache):
 
 
 def plot3d(a: NDArray, name: str, detector_z: float):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14.4, 7.2), dpi=150, subplot_kw={"projection": "3d"})
-    fig.suptitle(name)
+    fig, ax2 = plt.subplots(1, 1, figsize=(8, 8), dpi=200, subplot_kw={"projection": "3d"}, layout="tight")
+    # fig.suptitle(name)
     a = a[:: M.sample_size // 100]  # only take 100 element subset of sample
-    a[:, 1, :] = a[:, 1, :] / (C.m_pp * C.MeVperCsq2kg)
-    a[:, 2, :] = a[:, 2, :] / (C.m_np * C.MeVperCsq2kg)
+    # a[:, 1, :] = a[:, 1, :] / (C.m_pp * C.MeVperCsq2kg)
+    # a[:, 2, :] = a[:, 2, :] / (C.m_np * C.MeVperCsq2kg)
+    pos, mom, num, ipos, imom = extend_vectors(detector_z, a, True)
 
-    ax1.quiver(empty := np.zeros(a.shape[0]), empty, empty, a[:, 0, 0], a[:, 0, 1], a[:, 0, 2])
+    # ax1.quiver(
+    #     empty := np.zeros(pos.shape[0]),
+    #     empty,
+    #     empty,
+    #     pos[:, 0],
+    #     pos[:, 1],
+    #     pos[:, 2],
+    #     arrow_length_ratio=0,
+    # )
 
     r = np.linspace(0, E.DETECTOR2_RADIUS, 50)
     phi = np.linspace(0, 2 * np.pi, 50)
     x = np.outer(r, np.cos(phi))
     y = np.outer(r, np.sin(phi))
     z = np.full(x.shape, detector_z)
-    ax2.plot_surface(x, y, z, label="Detector", shade=False, antialiased=False, color="orange")
+    ax2.plot_surface(x, y, z, label="Detector", shade=False, antialiased=False, color="orange", alpha=0.3)
+    ax2.plot(pos[:, 0], pos[:, 1], pos[:, 2], "kx", markersize=5, label="Decay Vertices")
+    ax2.plot((ipos + imom)[:, 0], (ipos + imom)[:, 1], (ipos + imom)[:, 2], "rx", markersize=5, label="Intersection Points")
 
     ax2.quiver(
-        a[:, 0, 0],
-        a[:, 0, 1],
-        a[:, 0, 2],
-        a[:, 1, 0],
-        a[:, 1, 1],
-        a[:, 1, 2],
-        label="π⁺ velocities",
-        normalize=True,
-        length=1,
-    )
-    ax2.quiver(
-        a[:, 0, 0],
-        a[:, 0, 1],
-        a[:, 0, 2],
-        a[:, 2, 0],
-        a[:, 2, 1],
-        a[:, 2, 2],
-        label="π⁰ velocities",
-        normalize=True,
-        length=1,
+        pos[:, 0],
+        pos[:, 1],
+        pos[:, 2],
+        mom[:, 0],
+        mom[:, 1],
+        mom[:, 2],
+        arrow_length_ratio=0,
+        label="$\pi^+$ and $\pi^0$ flight directions",
+        alpha=0.5,
     )
 
     ax2.set_zlim(0, detector_z)
-    ax1.set_title("Kaon decay vertices in $m$")
-    ax2.set_title("Pion velocities in $ms^{-1}$")
-    ax2.legend()
-    ax1.view_init(210, 0, 90)
-    ax2.view_init(210, 0, 90)
-    fig.savefig(f"./graphs/task3_sample_{name.replace(' ', '_').lower()}.png")
+    # ax1.set_title("Kaon decay vertices in $m$")
+    ax2.set_title(f"{name} with {num} intersecting particles")
+    # ax1.view_init(200, 0, 90)
+    ax2.view_init(200, 0, 90)
+    ax2.legend(loc='upper left')
+
+    return fig
 
 
-def plot_samples(cache: Cache):
+def plot_samples(cache: Cache, interactive: bool = False):
     logger.info("Plotting not-divergent beam")
-    plot3d(cache.not_angled_sample, "Not divergent beam", cache.not_angled_ideal_z)
+    fig1 = plot3d(cache.not_angled_sample, "Not divergent beam", cache.not_angled_ideal_z)
     logger.info("Plotting divergent beam")
-    plot3d(cache.angled_sample, "Divergent sample", cache.angled_ideal_z)
+    fig2 = plot3d(cache.angled_sample, "Divergent beam", cache.angled_ideal_z)
+    if interactive:
+        plt.show()
+    else:
+        fig1.savefig(name := f"./graphs/task3_sample_not_divergent_beam.png")
+        logger.info(f"Plot saved to {name}")
+        fig2.savefig(name := f"./graphs/task3_sample_divergent_beam.png")
+        logger.info(f"Plot saved to {name}")
 
 
 def plot_samples_vispy(cache: Cache, interactive: bool = False):
@@ -143,9 +150,11 @@ def plot_samples_vispy(cache: Cache, interactive: bool = False):
         logger.info("Plots saved to ./graphs")
         app.quit()
 
-
-
-def extend_vectors(z: float, a: NDArray) -> tuple[NDArray, NDArray]:
+@overload
+def extend_vectors(z: float, a: NDArray, num: L[False]) -> tuple[NDArray, NDArray]: ...
+@overload
+def extend_vectors(z: float, a: NDArray, num: L[True]) -> tuple[NDArray, NDArray, int, NDArray, NDArray]: ...
+def extend_vectors(z: float, a: NDArray, num: bool = False) -> Union[tuple[NDArray, NDArray], tuple[NDArray, NDArray, int]]:
     """
     Extends the vectors in a sample to the detector.
     """
@@ -167,9 +176,14 @@ def extend_vectors(z: float, a: NDArray) -> tuple[NDArray, NDArray]:
 
     # Filter out the ones going backwards in time
     mask = travel_time > 0
-    travel_time, momentum_vecs, xy_offsets = travel_time[mask], momentum_vecs[mask], decays[mask]
+    travel_time, momentum_vecs, pos = travel_time[mask], momentum_vecs[mask], decays[mask]
 
-    return decays, momentum_vecs * travel_time[:, np.newaxis]
+    mask = np.sum((momentum_vecs[:, :2] * travel_time[:, np.newaxis] + pos[:, :2]) ** 2, axis=1) <= E.d2radsq
+    if num:
+        it, imom, ipos = travel_time[mask], momentum_vecs[mask], pos[mask]
+        return pos, momentum_vecs * travel_time[:, np.newaxis], np.sum(mask), ipos, imom*it[:, np.newaxis]
+
+    return pos, momentum_vecs * travel_time[:, np.newaxis]
 
 
 def create_canvas(sample: NDArray, detector_z: float):
@@ -184,12 +198,19 @@ def create_canvas(sample: NDArray, detector_z: float):
 
     arrows = np.hstack([pos, (end := pos + d)])
     logger.debug(f"{arrows.shape=}")
-    vpos = np.empty((2*pos.shape[0], 3), dtype=float)
+    vpos = np.empty((2 * pos.shape[0], 3), dtype=float)
     vpos[0::2] = pos
     vpos[1::2] = end
 
     arrow = scene.visuals.Arrow(
-        pos=vpos, arrows=arrows, connect="segments", arrow_type="angle_30", arrow_size=50, color="red", arrow_color="red", parent=view.scene
+        pos=vpos,
+        arrows=arrows,
+        connect="segments",
+        arrow_type="angle_30",
+        arrow_size=50,
+        color="red",
+        arrow_color="red",
+        parent=view.scene,
     )
 
     num_segments = 100
@@ -234,9 +255,9 @@ def main(args: argparse.Namespace) -> int:
     else:
         logger.info("Loading cache from file")
         cache = Cache(args.cache_file)
-    args.interactive = False
     plot_nll(cache, args.interactive)
-    plot_samples_vispy(cache, args.interactive)
+    plot_samples(cache, args.interactive)
+    # plot_samples_vispy(cache, args.interactive)
     return 0
 
 
@@ -245,6 +266,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-q", "--quiet", action="count", help="Decrease output verbosity.", default=0)
     parser.add_argument("-v", "--verbose", action="count", help="Increase output verbosity.", default=0)
+    parser.add_argument("-i", "--interactive", action="store_true", help="Interactive graphs.")
     group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument(
         "-c",
